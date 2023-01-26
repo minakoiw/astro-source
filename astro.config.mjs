699f5691338f8a341263237933b65a33563f29ce
Brian Yamasaki <brian.yamasaki@gmail.com>
2023-01-25 21:46:53 -0800

    Bolden Link on header if the current page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://minakoiw.github.io'
+  site: 'https://minakoiw.github.io',
+  build: {
+    format: 'directory'
+  }
 });
